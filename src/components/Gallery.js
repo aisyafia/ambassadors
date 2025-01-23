8774e2f74f2ee7459280340b7e7b00bcf96bc2be
aisyafia <aisya.lutfia@gmail.com>
2025-01-23 19:30:04 +0100

    feat: download button on image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,6 @@
 import { createUseStyles } from "react-jss";
+import { IoMdCloudDownload } from "react-icons/io";
+
 const useStyles = createUseStyles((theme) => ({
   Gallery: {
     marginTop: "2rem",
@@ -13,7 +15,6 @@ const useStyles = createUseStyles((theme) => ({
     background: theme.light.background,
     position: "relative",
     display: "flex",
-    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: "8px",
@@ -24,11 +25,41 @@ const useStyles = createUseStyles((theme) => ({
       textAlign: "center",
       width: "fit-content",
     },
+
+    "&:hover button": {
+      opacity: 1,
+      visibility: "visible",
+    },
+  },
+  Actions: {
+    //   display: "none",
+    marginLeft: 8,
+
+    "& button": {
+      padding: "6px 12px",
+      borderRadius: "4px",
+      background: "transparent",
+      border: "none",
+      cursor: "pointer",
+      opacity: 0, // Initially hidden
+      visibility: "hidden", // Hide from layout
+      transition: "opacity 0.3s ease, visibility 0.3s ease",
+      //   "&:hover": {
+      //     background: "#0056b3",
+      //   },
+    },
   },
 }));
 
 function Gallery({ pictures }) {
   const classes = useStyles();
+
+  const handleDownload = (dataUri, filename) => {
+    const link = document.createElement("a");
+    link.href = dataUri;
+    link.download = filename;
+    link.click();
+  };
 
   return (
     <section className={classes.Gallery}>
@@ -36,8 +67,19 @@ function Gallery({ pictures }) {
       {pictures &&
         pictures.map((picture) => (
           <div className={classes.Picture}>
-            <h3>{picture.title}</h3>
-            <img src={picture.dataUri} />
+            <div>
+              <h3>{picture.title}</h3>
+              <img src={picture.dataUri} />
+            </div>
+            <div className={classes.Actions}>
+              <button
+                onClick={() =>
+                  handleDownload(picture.dataUri, `${picture.title}.png`)
+                }
+              >
+                <IoMdCloudDownload size={"4rem"} color="#383c41" opacity={9} />
+              </button>
+            </div>
           </div>
         ))}
     </section>
